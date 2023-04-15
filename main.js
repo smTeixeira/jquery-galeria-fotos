4c396b7f844e7f868760cf47b935a0954518cebd
samuel de Matos Teixeira <samuelteixeira00@hotmail.com>
2023-04-15 18:59:33 -0300

    initial

---
+++ main.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    $('header button').click(function() {
+        $('form').slideDown();
+    })
+
+    $('#botao-cancelar').click(function() {
+        $('form').slideUp();
+    })
+
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+        const endereçoDaNovaImagem = $('#endereço-imagem-nova').val();
+        const novoItem = $('<li style="display: none"></li>');
+        $(`<img src="${endereçoDaNovaImagem}" />`).appendTo(novoItem);
+        $(`
+
+        <div class="overlay-imagem-link">
+            <a href="${endereçoDaNovaImagem}" target="_blank" Title="Ver imagem em tamanho real" >
+                Ver imagem em tamanho real
+            </a>
+        </div>
+        `).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn(1000);
+        $('#endereço-imagem-nova').val(' ')
+    })
+})
